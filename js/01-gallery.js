10a3701d51629e77ac9fdbf150c27452adb8aa45
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-04-18 21:03:50 +0200

    task 1, task 2 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,10 +4,28 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const imagesList = document.querySelector('ul.gallery');
-const newImage = galleryItems.map(
-    (image) => `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></li>`
-    );
+const newImage = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></li>`
+  )
+  .join('');
 
-  imagesList.insertAdjacentHTML('beforeend', newImage);
+imagesList.insertAdjacentHTML('beforeend', newImage);
 
-  imagesList.addEventListener('click', changeUrl);
+imagesList.addEventListener('click', fullImage);
+
+function fullImage(event) {
+  event.preventDefault();
+  const fullSizeImg = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${fullSizeImg}">
+`);
+  instance.show();
+  imagesList.addEventListener('keydown', closeByKeyDown);
+  function closeByKeyDown(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesList = document.querySelector('ul.gallery');
+const newImage = galleryItems
+  .map(
+    (image) =>
+      `<li><a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}" /></a></li>`
+  )
+  .join('');
+
+imagesList.insertAdjacentHTML('beforeend', newImage);
+
+imagesList.addEventListener('click', fullImage);
+
+function fullImage(event) {
+  event.preventDefault();
+  const fullSizeImg = new SimpleLightbox('.gallery a', { showCounter: false });
+}
